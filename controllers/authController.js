ad99fb115fc9944e18ddff6cc473e87f946f035d
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-21 10:35:43 -0400

    feat(administracion): Vista de administracion de vacantes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport')
+const mongoose = require('mongoose')
+const Vacante = mongoose.model('Vacante')
 
 exports.autenticarUsuario = passport.authenticate('local',{
     successRedirect: '/administracion',
@@ -6,3 +8,22 @@ exports.autenticarUsuario = passport.authenticate('local',{
     failureFlash: true,
     badRequestMessage: 'Ambos campos son obligatorios'
 })
+
+// Revisar si usuario esta autenticado o no
+exports.verificarUsuario = (req,res,next) => {
+    // revisar el usuario
+    if(req.isAuthenticated()){
+        return next() // Autenticado
+    } 
+    res.redirect('/iniciar-sesion')
+}
+
+exports.mostrarPanel = async (req,res) => {
+    // consultar el usuario autenticado
+    const vacantes = await Vacante.find({ autor: req.user._id})
+    res.render('administracion',{
+        nombrePagina: 'Panel de Administracion',
+        tagline: 'Crea y Administra tus vacantes desde aqui',
+        vacantes
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,27 @@ module.exports = () => {
     router.get('/', homeController.mostrarTrabajos)
 
     // Crear Vacantes
-    router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante)
-    router.post('/vacantes/nueva', vacantesController.agregarVacante)
+    router.get('/vacantes/nueva', 
+        authController.verificarUsuario,
+        vacantesController.formularioNuevaVacante
+    )
+    router.post('/vacantes/nueva', 
+        authController.verificarUsuario,
+        vacantesController.agregarVacante
+    )
     
     // Mostrar Vacante
     router.get('/vacantes/:url', vacantesController.mostrarVacante)
 
     // Editar vacante
-    router.get('/vacantes/editar/:url', vacantesController.formEditarVacante)
-    router.post('/vacantes/editar/:url', vacantesController.editarVacante)
+    router.get('/vacantes/editar/:url', 
+        authController.verificarUsuario,
+        vacantesController.formEditarVacante
+    )
+    router.post('/vacantes/editar/:url', 
+        authController.verificarUsuario,
+        vacantesController.editarVacante
+    )
 
     // Crear cuentas
     router.get('/crear-cuenta',usuariosController.formCrearCuenta)
@@ -26,9 +38,15 @@ module.exports = () => {
         usuariosController.crearUsuario
     )
 
-    // Autentica usuarios
+    // Autenticar usuarios
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion)
     router.post('/iniciar-sesion',authController.autenticarUsuario)
+
+    // Panel de administracion
+    router.get('/administracion',
+        authController.verificarUsuario,
+        authController.mostrarPanel
+    )
     
     return router;
 }
