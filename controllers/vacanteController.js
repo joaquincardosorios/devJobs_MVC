f9ff16b822da7ff0b831c9155dab99b5cc6f399c
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-24 15:50:34 -0400

    feat(vacantes): Validacion de creacion de vacantes

--- controllers/vacanteController.js
+++ controllers/vacanteController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Vacante = mongoose.model('Vacante')
+const { check, validationResult } =  require('express-validator')
 
 exports.formularioNuevaVacante = (req,res) => {
     res.render('nueva-vacante',{
@@ -58,4 +59,31 @@ exports.editarVacante = async (req,res) => {
     const vacante = await Vacante.findOneAndUpdate({url: req.params.url}, vacanteActualizada)
 
     res.redirect(`/vacantes/${vacante.url}`)
+}
+
+// Validar y sanitizar los campos de las nuevas vacantes
+exports.validarVacante = async (req,res,next) => {
+    console.log('hola')
+    await check('titulo').escape().notEmpty().withMessage('Agrega un titulo a la vacante').run(req)
+    await check('empresa').escape().notEmpty().withMessage('Agrega una empresa').run(req)
+    await check('ubicacion').escape().notEmpty().withMessage('Agrega una ubicacion').run(req)
+    await check('salario').escape().run(req)
+    await check('contrato').escape().notEmpty().withMessage('Selecciona el tipo de contrato').run(req)
+    await check('skills').escape().notEmpty().withMessage('Agrega al menos una habilidad').run(req)
+
+    const errores = validationResult(req).array()
+    // console.log(errores)
+    if(errores.length){
+        const cosa = req.flash('error', errores.map( error => error.msg))
+        res.render('nueva-vacante',{
+            nombrePagina: 'Nueva Vacante',
+            tagline: 'Llena el formulario y publica tu vacante',
+            cerrarSesion: true,
+            nombre: req.user.nombre,
+            mensajes: req.flash()
+        })
+        return
+    }
+    next()
+
 }
